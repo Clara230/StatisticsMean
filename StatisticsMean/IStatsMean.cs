9199599c05fb3b895c694b5d3bf0c5448fa75476
clara9621 <58587888+clara9621@users.noreply.github.com>
2020-01-15 11:27:41 +0200

    Add project files.

---
+++ StatisticsMean/IStatsMean.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Text;
+
+
+
+namespace StatisticsMean
+
+{
+
+    public interface IStatsMean
+
+    {
+
+        public dynamic Mean(dynamic a);
+
+    }
+
+}
---
+++ StatisticsMean/StatsMean.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+
+namespace StatisticsMean
+
+{
+
+    public class StatsMean : IStatsMean
+
+    {
+
+        public static dynamic result;
+
+
+
+        public dynamic Mean(dynamic a)
+
+        {
+
+
+
+            result = StatsOperations.StatisticsMean.Mean(a);
+
+            return result;
+
+        }
+
+    }
+
+}
